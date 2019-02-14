cb36f7bd89b556e8de41cbdff6c55aca8aaf6fdb
Andrei Fidelman <fidelmanlife@gmail.com>
2019-02-14 19:58:12 +0100

    init

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import TransitionAReactComponentWithCSSTransition from './lesson';
+
+class App extends Component {
+  render() {
+    return (
+      <TransitionAReactComponentWithCSSTransition />
+    );
+  }
+}
+
+export default App;
